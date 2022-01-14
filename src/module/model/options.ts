5c55e60a6e569151aac9bd6341df286a0f45d000
dvillegas <dvillegas@hotelbeds.com>
2022-01-14 15:35:39 +0100

    add image filepickers on options dialog

--- src/module/model/options.ts
+++ src/module/model/options.ts
@@ -1,3 +1,5 @@
+import { Utils } from '../utils';
+
 export class Options {
     generateSectorCoordinates = true;
     generateNotesForAllEntities = false;
@@ -5,4 +7,20 @@ export class Options {
     addTypeToEntityJournal = true;
     generateHexName = true;
     prefixSystemFoldersWithCoordinates = true;
+
+    asteroidBasePath = Utils.getImagePath("asteroidBase.png");
+    asteroidBeltPath = Utils.getImagePath("asteroidBelt.png");
+    blackHolePath = Utils.getImagePath("blackHole.png");
+    deepSpaceStationPath = Utils.getImagePath("deepSpaceStation.png");
+    gasGiantMinePath = Utils.getImagePath("gasGiantMine.png");
+    moonPath = Utils.getImagePath("moon.png");
+    moonBasePath = Utils.getImagePath("moonBase.png");
+    orbitalRuinPath = Utils.getImagePath("orbitalRuin.png");
+    planetPath = Utils.getImagePath("planet.png");
+    refuelingStationPath = Utils.getImagePath("refuelingStation.png");
+    researchBasePath = Utils.getImagePath("researchBase.png");
+    spaceStationPath = Utils.getImagePath("spaceStation.png");
+    systemPath = Utils.getImagePath("system.png");
+
+    backgroundPath = Utils.getImagePath("starField.png");
 }
